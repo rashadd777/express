575e7ce016a4309ba2ed3bb6c8e2d4ee2b29dfe0
rashadd777 <105500466+rashadd777@users.noreply.github.com>
2022-12-23 01:07:34 +0400

    Add files via upload

---
+++ express/README.md
@@ -0,0 +1,2 @@
+# M5-D3-T1
+https://docs.google.com/presentation/d/1gbrg7zkptKx75n0NrTkrgv5x3SLNVKmnonaKnc0HRaA/edit#slide=id.p14
---
+++ express/main.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+app.get('/', function (req, res) {
+    res.send('Izzet Omarov');
+  });
+app.get('/card', function (req, res) {
+  res.send('Bu route kartlar üçün cavabdehdir');
+});
+app.get('/client', function (req, res) {
+  res.send('Bu marşrut müştərilər üçün cavabdehdir');
+});
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
